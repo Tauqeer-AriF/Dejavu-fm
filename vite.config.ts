6d4be95bb5aa14229bc2a1a9fbbd12f092ab6117
Tauqeer Arif <49623388+Tauqeer-AriF@users.noreply.github.com>
2026-06-16 22:54:27 +0500

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -99,19 +99,14 @@ export default defineConfig(({mode}) => {
           icons: [
             {
               src: 'icon.svg',
-              sizes: '192x192',
+              sizes: 'any',
               type: 'image/svg+xml'
             },
             {
               src: 'icon.svg',
-              sizes: '512x512',
-              type: 'image/svg+xml'
-            },
-            {
-              src: 'icon.svg',
-              sizes: '512x512',
+              sizes: '192x192 512x512',
               type: 'image/svg+xml',
-              purpose: 'any maskable'
+              purpose: 'maskable'
             }
           ]
         }
