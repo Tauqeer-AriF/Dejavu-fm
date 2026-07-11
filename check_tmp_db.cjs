57df78a3cdf14213a8a94af0603b0f369d2d294e
Tauqeer Arif <49623388+Tauqeer-AriF@users.noreply.github.com>
2026-07-11 14:34:52 +0500

    Delete check_tmp_db.cjs

--- check_tmp_db.cjs
+++
@@ -1,8 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('/tmp/dejavufm.db');
-try {
-  const tables = db.prepare("SELECT name FROM sqlite_master WHERE type='table'").all();
-  console.log("Tables:", tables.map(t => t.name));
-} catch (e) {
-  console.error("Failed:", e.message);
-}
