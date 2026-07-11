1a07f8145e5e93799c50e5c059a49173bb475582
Tauqeer Arif <49623388+Tauqeer-AriF@users.noreply.github.com>
2026-07-11 14:34:37 +0500

    Delete check_db.cjs

--- check_db.cjs
+++
@@ -1,8 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('dejavufm.db');
-try {
-  const metadata = db.prepare("SELECT * FROM backup_metadata").all();
-  console.log("Metadata:", JSON.stringify(metadata, null, 2));
-} catch (e) {
-  console.error("Query failed:", e.message);
-}
