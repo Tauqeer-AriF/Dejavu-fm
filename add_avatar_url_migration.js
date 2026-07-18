7eded006080338f69c9c88141a6cc885af6674a6
Tauqeer Arif <49623388+Tauqeer-AriF@users.noreply.github.com>
2026-07-18 08:36:18 +0500

    Delete add_avatar_url_migration.js

--- add_avatar_url_migration.js
+++
@@ -1,8 +0,0 @@
-import Database from 'better-sqlite3';
-const db = new Database('database.db');
-try {
-  db.prepare("ALTER TABLE public_messages ADD COLUMN avatar_url TEXT;").run();
-  console.log("Migration successful");
-} catch (e) {
-  console.log(e.message);
-}
