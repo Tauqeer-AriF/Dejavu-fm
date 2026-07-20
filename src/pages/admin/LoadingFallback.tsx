36ee9dfe008c21d3df7da36057fe7b3cc5782d99
Tauqeer Arif <49623388+Tauqeer-AriF@users.noreply.github.com>
2026-07-20 16:45:02 +0500

    Delete src directory

--- src/pages/admin/LoadingFallback.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function LoadingFallback() {
-  return (
-    <div className="flex items-center justify-center min-h-[400px]">
-      <div className="w-8 h-8 border-4 border-neon-purple rounded-full animate-spin shadow-[0_0_15px_rgba(176,38,255,0.5)]" />
-    </div>
-  );
-}
