c0af6a88e4187cded86650ce6110add142de0b8b
Tauqeer Arif <49623388+Tauqeer-AriF@users.noreply.github.com>
2026-07-23 00:35:21 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(() => {
       react(), 
       tailwindcss(),
       VitePWA({
-        registerType: 'autoUpdate',
+        registerType: 'prompt',
         manifestFilename: 'manifest.json',
         includeAssets: [
           'favicon.ico',
